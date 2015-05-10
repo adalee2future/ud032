509a51a0037e7a83c8ebf0d266374d260ef2f22c
Ada Lee <lili545442483@163.com>
2015-05-10 21:02:53 +0800

    My solution to parsing csv file

--- Lesson_1_Data_Extraction_Fundamentals/07-Parsing_CSV_Files/simple.py
+++ Lesson_1_Data_Extraction_Fundamentals/07-Parsing_CSV_Files/simple.py
@@ -15,19 +15,24 @@ DATAFILE = "beatles-diskography.csv"
 
 def parse_file(datafile):
     data = []
+    first_line = True
     with open(datafile, "rb") as f:
         for line in f:
-            print line
-
+            if first_line:
+                headers = line.rstrip().split(",")
+                first_line = False
+            else:
+                contents = line.rstrip().split(",")
+                header_vs_content = {header : content for header, content in zip(headers, contents)}
+                data.append(header_vs_content)
     return data
-
 
 def test():
     # a simple test of your implemetation
     datafile = os.path.join(DATADIR, DATAFILE)
     d = parse_file(datafile)
-    firstline = {'Title': 'Please Please Me', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '22 March 1963', 'US Chart Position': '-', 'RIAA Certification': 'Platinum', 'BPI Certification': 'Gold'}
-    tenthline = {'Title': '', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '10 July 1964', 'US Chart Position': '-', 'RIAA Certification': '', 'BPI Certification': 'Gold'}
+    firstline = {'Title': 'Please Please Me', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '22 March 1963', 'US Chart Position': '-', 'RIAA Certification': 'Platinum', 'BPI Certification': 'Gold'} 
+    tenthline = {'Title': '', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '10 July 1964', 'US Chart Position': '-', 'RIAA Certification': '', 'BPI Certification': 'Gold'} 
 
     assert d[0] == firstline
     assert d[9] == tenthline
