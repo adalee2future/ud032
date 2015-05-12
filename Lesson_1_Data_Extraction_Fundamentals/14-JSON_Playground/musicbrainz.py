918142a12a1f31a6c65f87f7cc9af63ae48bf802
Ada Lee <lili545442483@163.com>
2015-05-12 19:34:51 +0800

    My code to retrieval json data from the internet

--- Lesson_1_Data_Extraction_Fundamentals/14-JSON_Playground/musicbrainz.py
+++ Lesson_1_Data_Extraction_Fundamentals/14-JSON_Playground/musicbrainz.py
@@ -1,6 +1,6 @@
 # To experiment with this code freely you will have to run this code locally.
-# We have provided an example json output here for you to look at,
-# but you will not be able to run any queries through our UI.
+# We have provided an example json output here for you to look at, 
+# but you will not be able to run any queries through our UI. 
 import json
 import requests
 
@@ -8,14 +8,14 @@ import requests
 BASE_URL = "http://musicbrainz.org/ws/2/"
 ARTIST_URL = BASE_URL + "artist/"
 
-query_type = {  "simple": {},
+query_type = {  "simple": {}, 
                 "atr": {"inc": "aliases+tags+ratings"},
                 "aliases": {"inc": "aliases"},
                 "releases": {"inc": "releases"}}
 
 
 def query_site(url, params, uid="", fmt="json"):
-    params["fmt"] = fmt
+    params["fmt"] = fmt 
     r = requests.get(url + uid, params=params)
     print "requesting", r.url
 
@@ -38,22 +38,54 @@ def pretty_print(data, indent=4):
 
 
 def main():
-    results = query_by_name(ARTIST_URL, query_type["simple"], "Nirvana")
-    pretty_print(results)
+    # Question 1: HOW MANY BANDS NAMED "FIRST AID KIT"?
+    print("Question 1") 
+    results = query_by_name(ARTIST_URL, query_type["simple"], "FIRST AID KIT")
+    count = 0 
+    for artist in results["artists"]:
+        if artist["name"].upper() == "FIRST AID KIT":
+            count += 1
+    print "Question: How many bands named FIRST AID KIT? Answer: %i.\n" % count
 
-    artist_id = results["artists"][1]["id"]
-    print "\nARTIST:"
-    pretty_print(results["artists"][1])
+    # Question 2: BEGIN-AREA NAME FOR QUEEN?
+    print("Question 2") 
+    results = query_by_name(ARTIST_URL, query_type["simple"], "QUEEN")
+    for artist in results["artists"]:
+        if artist["name"].upper() == "QUEEN":
+            begin_area_name = artist["begin-area"]["name"]
+            break
+    print "Question: BEGIN-AREA NAME FOR QUEEN? Answer: %s.\n" % begin_area_name
 
-    artist_data = query_site(ARTIST_URL, query_type["releases"], artist_id)
-    releases = artist_data["releases"]
-    print "\nONE RELEASE:"
-    pretty_print(releases[0], indent=2)
-    release_titles = [r["title"] for r in releases]
+    # Question 3: SPANISH ALIAS FOR BEATTLES?
+    print("Question 3") 
+    results = query_by_name(ARTIST_URL, query_type["simple"], "BEATLES")
+    for artist in results["artists"]:
+        if artist["name"].upper() == "THE BEATLES":
+            for alias in artist["aliases"]:
+                if alias["locale"] == "es":
+		    spanish_name = alias["name"]
+		    break
+            break
+    print "Question: SPANISH ALIAS FOR BEATLES? Answer: %s.\n" % spanish_name
 
-    print "\nALL TITLES:"
-    for t in release_titles:
-        print t
+    # Question 4: NIRUANA DISAMBIGUATION?
+    print("Question 4") 
+    results = query_by_name(ARTIST_URL, query_type["simple"], "NIRVANA")
+    for artist in results["artists"]:
+        if artist["name"].upper() == "NIRVANA":
+	    disambiguation = artist["disambiguation"]
+            break
+    print "Question: BEGIN-AREA NAME FOR QUEEN? Answer: %s.\n" % disambiguation
+
+
+    # Question 5: WHEN WAS ONE DIRECTION FORMED?
+    print("Question 5") 
+    results = query_by_name(ARTIST_URL, query_type["simple"], "ONE DIRECTION")
+    for artist in results["artists"]:
+        if artist["name"].upper() == "ONE DIRECTION":
+	    year = artist["life-span"]["begin"]
+            break
+    print "Question: BEGIN-AREA NAME FOR QUEEN? Answer: %s.\n" % year
 
 
 if __name__ == '__main__':
